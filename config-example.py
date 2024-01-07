c3b2ed0dd65097e9a2a2904d26db2bdf12a75202
CheeseLad <cheesescreativeserver@gmail.com>
2024-01-07 03:15:56 +0000

    committing assets

---
+++ config-example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# This file contains all the configuration variables for the newsletter
+
+newsletter_colour = '#ffffff' # Colour of the newsletter buttons
+email_colour = '#ffffff' # Colour of the email footer text #ff
+author_info = 'John Doe' # Name
+author_year = 'Secretary 2023-2024' # Year and Position
+facebook_img = 'https://url.com/facebook.png' # Replace with your own images
+twitter_img = 'https://url.com/twitter.png' # Replace with your own images
+instagram_img = 'https://url.com/instagram.png' # Replace with your own images
+discord_img = 'https://url.com/discord.png' # Replace with your own images
