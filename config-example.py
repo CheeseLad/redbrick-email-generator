45d820f53dc5316ae133fd2e0dc4f9e349e51354
CheeseLad <cheesescreativeserver@gmail.com>
2024-04-27 01:41:03 +0100

    automate sending newsletters with one command

--- config-example.py
+++ config-example.py
@@ -10,3 +10,11 @@ facebook_img = 'https://url.com/facebook.png' # Replace with your own images
 twitter_img = 'https://url.com/twitter.png' # Replace with your own images
 instagram_img = 'https://url.com/instagram.png' # Replace with your own images
 discord_img = 'https://url.com/discord.png' # Replace with your own images
+
+# Email configuration
+
+sender_email = 'example@example.com' # Email address
+sender_password = 'password' # Email password
+smtp_server = 'smtp.example.com' # SMTP server
+smtp_port = 465 # SMTP port
+recipients = ['example1@example.com', 'example2@example.com'] # Recipient email addresses
